c838dcd5ff90509bffdee7d1a32e9172d9489a08
+ <+>
2021-07-02 08:29:48 -0700

    commit tree 2e42c060b6687e251ad2b3c5a2c6d6652dee6bd1

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((acc, cv) => cv + acc)
+
+// reduce:
+// takes a callback (like iterators)
+// the callback takes params of its own
+// the callbakc's params include:
+    // 1) an accumulator (keeps track of the accumulated value)
+    // 2) current value (like all other iterators)
